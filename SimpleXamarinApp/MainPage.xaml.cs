aec15ef3a2a2da7380211841fcddd18965fc6605
Ярослав Троцький <trotskyi.yaroslav@gmail.com>
2023-03-29 11:46:35 +0300

    Add project files.

---
+++ SimpleXamarinApp/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using XamarinApp;
+
+namespace SimpleXamarinApp
+{
+    public partial class MainPage : ContentPage
+    {
+        List<Report> reports = new List<Report>();
+        public MainPage()
+        {
+            InitializeComponent();
+            for (int i = 1; i < 32; i++)
+            {
+                reports.Add(new Report(i, "Cloudy", new DateTime(2023, 03, i)));
+            }
+        }
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            var text = ((Entry)sender).Text;
+            foreach (var item in reports)
+            {
+                if (item.uid.ToString() == text)
+                {
+                    CounterLabel.Text = item.uid.ToString() + " | " + item.report + " | " + item.date.ToString();
+                }
+            }
+        }
+    }
+}
---
+++ SimpleXamarinApp/Report.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinApp
+{
+    public class Report
+    {
+        public int uid { get; set; }
+        public string report { get; set; }
+        public DateTime date { get; set; }
+        public Report(int id, string r, DateTime d)
+        {
+            uid = id;
+            report = r;
+            date = d;
+        }
+
+    }
+}
